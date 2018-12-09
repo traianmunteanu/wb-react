d3ccd4543a5b294df6f35455274118238972153a
traianmunteanu <hello@traianmunteanu.com>
2018-12-09 21:39:51 +0200

    Added Order feature.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -18,19 +18,30 @@ class App extends React.Component {
     // Set the new fishes object to state
     this.setState({ fishes });
   };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes })
   };
+
+  addToOrder = key  => {
+    // take a copy of state
+    const order = { ...this.state.order };
+    // either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // call setState to update our state object
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>  
     )
