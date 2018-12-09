e5272067690dda5528f5513fd945793192cab5dc
traianmunteanu <hello@traianmunteanu.com>
2018-12-09 19:14:26 +0200

    Adding Routing, card rendering and a bunch of aux functionality

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // Take a copy of the existing state/clone an object
+    const fishes = { ...this.state.fishes };
+    // Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set the new fishes object to state
+    this.setState({ fishes });
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+      </div>  
+    )
+  }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h2>We couldn't find the page you're looking for.</h2>
+  </div>
+)
+
+export default NotFound;
